4d11f0d27651d8cb2269bc52ec1f641ade04aade
Leon <lixiao.dev@gmail.com>
2022-12-24 10:03:31 +0800

    fix: webembed crash on Android

--- packages/kit/src/views/ChainWebEmbed/index.tsx
+++ packages/kit/src/views/ChainWebEmbed/index.tsx
@@ -1,15 +1,41 @@
-import { memo, useMemo } from 'react';
+import { memo, useEffect, useMemo, useState } from 'react';
 
 import { useGeneral } from '@onekeyhq/kit/src/hooks/redux';
+import {
+  AppUIEventBusNames,
+  appUIEventBus,
+} from '@onekeyhq/shared/src/eventBus/appUIEventBus';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import { CardanoWebEmbedView } from './CardanoWebEmbedView';
 
 function ChainWebEmbed() {
   const { activeNetworkId } = useGeneral();
+  const [webEmbedDisabled, setWebEmbedDisabled] = useState(false);
+
+  useEffect(() => {
+    const onCloseChainWebEmbed = (disabled: boolean) => {
+      setWebEmbedDisabled(disabled);
+    };
+    appUIEventBus.on(
+      AppUIEventBusNames.ChainWebEmbedDisabled,
+      onCloseChainWebEmbed,
+    );
+    return () => {
+      appUIEventBus.off(
+        AppUIEventBusNames.ChainWebEmbedDisabled,
+        onCloseChainWebEmbed,
+      );
+    };
+  }, []);
 
   const content = useMemo(() => {
     if (!platformEnv.isNative) return null;
+
+    if (webEmbedDisabled) {
+      console.log('distroy webview');
+      return null;
+    }
 
     if (!activeNetworkId) return null;
 
@@ -17,7 +43,7 @@ function ChainWebEmbed() {
       return <CardanoWebEmbedView />;
     }
     return null;
-  }, [activeNetworkId]);
+  }, [activeNetworkId, webEmbedDisabled]);
 
   return content;
 }

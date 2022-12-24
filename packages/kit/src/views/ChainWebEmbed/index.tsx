de8a7f3efa2db67e91d49e26cc25cd5fd2e3590c
Leon <lixiao.dev@gmail.com>
2022-12-24 16:13:10 +0800

    fix: lazy load webembed

--- packages/kit/src/views/ChainWebEmbed/index.tsx
+++ packages/kit/src/views/ChainWebEmbed/index.tsx
@@ -1,23 +1,34 @@
-import { memo, useMemo } from 'react';
+import { memo, useEffect, useMemo, useState } from 'react';
 
 import { useGeneral } from '@onekeyhq/kit/src/hooks/redux';
+import debugLogger from '@onekeyhq/shared/src/logger/debugLogger';
 import platformEnv from '@onekeyhq/shared/src/platformEnv';
 
 import { CardanoWebEmbedView } from './CardanoWebEmbedView';
 
 function ChainWebEmbed() {
   const { activeNetworkId } = useGeneral();
+  const [lazyLoad, setLazyLoad] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLazyLoad(true);
+    }, 500);
+  }, []);
 
   const content = useMemo(() => {
     if (!platformEnv.isNative) return null;
 
+    debugLogger.common.debug('chainWebEmbed Rerender ========>>>>>');
+
     if (!activeNetworkId) return null;
 
-    if (activeNetworkId === 'ada--0') {
+    if (activeNetworkId === 'ada--0' && lazyLoad) {
+      debugLogger.common.debug('webembed Rerender ========<<<<<<');
       return <CardanoWebEmbedView />;
     }
     return null;
-  }, [activeNetworkId]);
+  }, [activeNetworkId, lazyLoad]);
 
   return content;
 }

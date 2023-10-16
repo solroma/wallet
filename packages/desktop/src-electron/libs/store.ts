c7337c2f915a69afb1591cad6885d7a1fab91c22
Leon <lixiao@onekey.so>
2023-10-16 14:24:37 +0800

    chore: update version

--- packages/desktop/src-electron/libs/store.ts
+++ packages/desktop/src-electron/libs/store.ts
@@ -1,4 +1,5 @@
 import { safeStorage } from 'electron';
+import logger from 'electron-log';
 import Store from 'electron-store';
 
 const store = new Store();
@@ -30,6 +31,11 @@ export const getSecureItem = (key: string) => {
   const item = store.get(EncryptedData, {}) as Record<string, string>;
   const value = item[key];
   if (value) {
+    const isEncryptionAvailable = safeStorage.isEncryptionAvailable();
+    logger.debug(
+      '=>>>: getSecureItem isEncryptionAvailable : ',
+      isEncryptionAvailable,
+    );
     const result = safeStorage.decryptString(Buffer.from(value, 'hex'));
     return result;
   }
@@ -37,6 +43,11 @@ export const getSecureItem = (key: string) => {
 
 export const setSecureItem = (key: string, value: string): void => {
   const items = store.get(EncryptedData, {}) as Record<string, string>;
+  const isEncryptionAvailable = safeStorage.isEncryptionAvailable();
+  logger.debug(
+    '=>>>: setSecureItem isEncryptionAvailable : ',
+    isEncryptionAvailable,
+  );
   items[key] = safeStorage.encryptString(value).toString('hex');
   store.set(EncryptedData, items);
 };

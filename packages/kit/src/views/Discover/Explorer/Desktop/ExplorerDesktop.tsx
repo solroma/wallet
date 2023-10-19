3a18af56772e660b458b55d121b0d7c1e1c85106
Leon <lixiao@onekey.so>
2023-10-19 15:44:09 +0800

    feat: web tabs manage

--- packages/kit/src/views/Discover/Explorer/Desktop/ExplorerDesktop.tsx
+++ packages/kit/src/views/Discover/Explorer/Desktop/ExplorerDesktop.tsx
@@ -1,6 +1,7 @@
 import { Stack, useThemeValue } from '@onekeyhq/components';
 import useSafeAreaInsets from '@onekeyhq/components/src/Provider/hooks/useSafeAreaInsets';
 
+import { withProviderWebTabs } from '../Context/contextWebTabs';
 import { webHandler } from '../explorerUtils';
 
 import ControllerBarDesktop from './ControllerBarDesktop';
@@ -8,18 +9,23 @@ import TabBarDesktop from './TabBarDesktop';
 
 const showExplorerBar = webHandler !== 'browser';
 
-function ExplorerDesktop() {
+function ExplorerHeaderCmp() {
   const { top } = useSafeAreaInsets();
   const tabBarBgColor = useThemeValue('bgSubdued') as string;
-  console.log(tabBarBgColor);
+  return (
+    <Stack mt={`${top ? top + 10 : 0}px`} bg={tabBarBgColor} zIndex={5}>
+      <TabBarDesktop />
+      <ControllerBarDesktop />
+    </Stack>
+  );
+}
+
+const ExplorerHeader = withProviderWebTabs(ExplorerHeaderCmp);
+
+function ExplorerDesktop() {
   return (
     <Stack flex={1} zIndex={3}>
-      {!showExplorerBar && (
-        <Stack mt={`${top ? top + 10 : 0}px`} bg={tabBarBgColor} zIndex={5}>
-          <TabBarDesktop />
-          <ControllerBarDesktop />
-        </Stack>
-      )}
+      {!showExplorerBar && <ExplorerHeader />}
       <Stack>WebView Content</Stack>
     </Stack>
   );
--- packages/kit/src/views/Discover/Explorer/Desktop/TabBarDesktop.tsx
+++ packages/kit/src/views/Discover/Explorer/Desktop/TabBarDesktop.tsx
@@ -1,99 +1,126 @@
+import { useCallback } from 'react';
+
 import { Image } from 'react-native';
 
 import { Button, Stack, Text } from '@onekeyhq/components';
+import { DebugRenderTracker } from '@onekeyhq/components/src/DebugRenderTracker';
 import dAppFavicon from '@onekeyhq/kit/assets/dapp_favicon.png';
+import platformEnv from '@onekeyhq/shared/src/platformEnv';
+
+import {
+  addBlankWebTabAtomWithWriteOnly,
+  atomWebTabs,
+  atomWebTabsMap,
+  closeWebTabAtomWithWriteOnly,
+  setCurrentWebTabAtomWithWriteOnly,
+  useAtomWebTabs,
+} from '../Context/contextWebTabs';
+import { dismissWebviewKeyboard } from '../explorerUtils';
 
 import type { LayoutChangeEvent } from 'react-native';
 
-export interface WebTab {
-  id: string;
-  url: string;
-  // urlToGo?: string;
-  title?: string;
-  favicon?: string;
-  thumbnail?: string;
-  // isPinned: boolean;
-  isCurrent: boolean;
-  isBookmarked?: boolean;
-  // isMuted: boolean;
-  canGoBack?: boolean;
-  canGoForward?: boolean;
-  loading?: boolean;
-  refReady?: boolean;
-  timestamp?: number;
-}
 function Tab({
-  isCurrent,
-  id,
-  title,
-  favicon,
+  tabId,
   onLayout,
-}: WebTab & {
+}: {
+  tabId: string;
   onLayout?: (e: LayoutChangeEvent) => void;
 }) {
-  return id === 'home' ? (
-    <Button
-      buttonVariant="primary"
-      borderRadius={0}
-      bg={isCurrent ? '$bg' : '$bgHover'}
-    >
-      <Button.Icon name="HomeMini" style={{ width: 16, height: 16 }} />
-    </Button>
-  ) : (
-    <Stack
-      height="32px"
-      hoverStyle={{
-        bg: isCurrent ? '$bg' : '$bgPrimaryHover',
-      }}
-      borderRightColor="$border"
-      borderRightWidth="0.5px"
-      px="$3"
-      bg={isCurrent ? '$bg' : '$bgHover'}
-      onPress={() => console.log('setCurrentTab')}
-      flexDirection="row"
-      justifyContent="space-between"
-      alignItems="center"
-      onLayout={onLayout}
-    >
-      <Image
-        style={{ width: 16, height: 16, marginRight: 8 }}
-        source={{ uri: favicon }}
-        defaultSource={dAppFavicon}
-      />
-      <Text
-        maxWidth="82px"
-        marginRight="10px"
-        color={isCurrent ? '$text' : '$textSubdued'}
-        variant="$bodySmMedium"
-      >
-        {title}
-      </Text>
-      <Button
-        size="small"
-        onPress={(e) => {
-          e.stopPropagation();
-          console.log('closeTab');
-        }}
-      >
-        <Button.Icon name="XMarkMini" />
-      </Button>
-    </Stack>
+  const [map] = useAtomWebTabs(atomWebTabsMap);
+  const tab = map[tabId];
+  const { isCurrent, title, favicon } = tab;
+  const [, setCurrentWebTabAction] = useAtomWebTabs(
+    setCurrentWebTabAtomWithWriteOnly,
+  );
+  const [, closeWebTab] = useAtomWebTabs(closeWebTabAtomWithWriteOnly);
+  const setCurrentTab = useCallback(() => {
+    if (platformEnv.isNative) {
+      dismissWebviewKeyboard();
+    }
+    setCurrentWebTabAction(tabId);
+  }, [tabId, setCurrentWebTabAction]);
+  const closeTab = useCallback(() => {
+    closeWebTab(tabId);
+  }, [tabId, closeWebTab]);
+
+  return (
+    <DebugRenderTracker>
+      {tabId === 'home' ? (
+        <Button
+          buttonVariant="primary"
+          borderRadius={0}
+          bg={isCurrent ? '$bg' : '$bgHover'}
+          onPress={setCurrentTab}
+        >
+          <Button.Icon name="HomeMini" style={{ width: 16, height: 16 }} />
+        </Button>
+      ) : (
+        <Stack
+          height="32px"
+          hoverStyle={{
+            bg: isCurrent ? '$bg' : '$bgPrimaryHover',
+          }}
+          borderRightColor="$border"
+          borderRightWidth="0.5px"
+          px="$3"
+          bg={isCurrent ? '$bg' : '$bgHover'}
+          onPress={setCurrentTab}
+          flexDirection="row"
+          justifyContent="space-between"
+          alignItems="center"
+          onLayout={onLayout}
+        >
+          <Image
+            style={{ width: 16, height: 16, marginRight: 8 }}
+            source={{ uri: favicon }}
+            defaultSource={dAppFavicon}
+          />
+          <Text
+            maxWidth="82px"
+            marginRight="10px"
+            color={isCurrent ? '$text' : '$textSubdued'}
+            variant="$bodySmMedium"
+          >
+            {title}
+          </Text>
+          <Button
+            size="small"
+            onPress={(e) => {
+              e.stopPropagation();
+              closeTab();
+            }}
+          >
+            <Button.Icon name="XMarkMini" />
+          </Button>
+        </Stack>
+      )}
+    </DebugRenderTracker>
   );
 }
 
+const AddTabButton = () => {
+  const [, addBlankWebTab] = useAtomWebTabs(addBlankWebTabAtomWithWriteOnly);
+  return (
+    <Button
+      borderRadius={0}
+      onPress={() => {
+        addBlankWebTab();
+      }}
+    >
+      <Button.Icon name="PlusMini" />
+    </Button>
+  );
+};
+
 function TabBarDesktop() {
-  const tabs: WebTab[] = Array.from({ length: 10 }).map((_, index) => ({
-    isCurrent: false,
-    id: index === 0 ? 'home' : index.toString(),
-    url: 'https://google.com',
-    title: 'Google',
-    favicon: 'https://www.google.com/favicon.ico',
-  }));
+  const [tabs] = useAtomWebTabs(atomWebTabs);
+  console.log('tabs ===>: ', tabs);
   return (
     <Stack flexDirection="row" w="100%" h="$8" alignItems="center">
-      {tabs.map((tab, index) => (
-        <Tab key={index} {...tab} />
+      {tabs.map((tab) => (
+        <Tab key={tab.id} tabId={tab.id} />
       ))}
+      <AddTabButton />
     </Stack>
   );
 }

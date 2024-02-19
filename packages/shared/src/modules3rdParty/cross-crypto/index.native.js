0e3580445e12023b42dce84fd6ada3c559c3569f
huhuanming <huanming@onekey.so>
2024-02-19 11:31:40 +0800

    Update index.native.js

--- packages/shared/src/modules3rdParty/cross-crypto/index.native.js
+++ packages/shared/src/modules3rdParty/cross-crypto/index.native.js
@@ -4,10 +4,10 @@
 //      (react-native-crypto depend on) react-native-randombytes (deprecated)
 console.log('================ cross-crypto (native)');
 
-if (global.crypto && global.crypto.getRandomValues) {
-  delete global.crypto.getRandomValues;
-}
-// shim global.crypto.getRandomValues
+// if (global.crypto && global.crypto.getRandomValues) {
+//   delete global.crypto.getRandomValues;
+// }
+// // shim global.crypto.getRandomValues
 // require('react-native-get-random-values');
 
 // if (process.env.NODE_ENV !== 'production') {
@@ -23,19 +23,19 @@ if (global.crypto && global.crypto.getRandomValues) {
 
 const crypto = require('react-native-crypto');
 
-const { randomBytes } = require('@noble/hashes/utils');
+// const { randomBytes } = require('@noble/hashes/utils');
 
 // re-assign randomBytes from global.crypto.getRandomValues
-crypto.randomBytes = randomBytes;
-crypto.getRandomValues =
-  crypto.getRandomValues || global.crypto.getRandomValues;
-global.crypto.randomBytes = global.crypto.randomBytes || crypto.randomBytes;
+// crypto.randomBytes = randomBytes;
+// crypto.getRandomValues =
+//   crypto.getRandomValues || global.crypto.getRandomValues;
+// global.crypto.randomBytes = global.crypto.randomBytes || crypto.randomBytes;
 
 crypto.$$isOneKeyShim = true;
 global.crypto.$$isOneKeyShim = true;
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('react-native-crypto polyfilled', crypto, global.crypto);
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   console.log('react-native-crypto polyfilled', crypto, global.crypto);
+// }
 
 module.exports = crypto;
